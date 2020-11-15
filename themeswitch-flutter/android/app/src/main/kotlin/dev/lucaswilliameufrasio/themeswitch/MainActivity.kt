fd703e7443f8d09dd3d2b22dab1448258ca669b9
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-15 00:57:15 -0300

    feat: add Theme Switch app built in with Flutter

---
+++ themeswitch-flutter/android/app/src/main/kotlin/dev/lucaswilliameufrasio/themeswitch/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.lucaswilliameufrasio.themeswitch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ themeswitch-flutter/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:provider/provider.dart';
+
+import 'package:themeswitch/models_providers/theme_provider.dart';
+import 'package:themeswitch/pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await pathProvider.getApplicationDocumentsDirectory();
+
+  Hive.init(appDocumentDirectory.path);
+
+  final settings = await Hive.openBox('settings');
+  bool isLightTheme = settings.get('isLightTheme') ?? false;
+
+  print(isLightTheme);
+
+  runApp(ChangeNotifierProvider(
+    create: (_) => ThemeProvider(isLightTheme: isLightTheme),
+    child: AppStart(),
+  ));
+}
+
+class AppStart extends StatelessWidget {
+  const AppStart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
+    return MyApp(
+      themeProvider: themeProvider,
+    );
+  }
+}
+
+class MyApp extends StatefulWidget with WidgetsBindingObserver {
+  final ThemeProvider themeProvider;
+
+  const MyApp({
+    Key key,
+    @required this.themeProvider,
+  }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Light Dark Theme Switch',
+      theme: widget.themeProvider.themeData(),
+      home: HomePage(),
+    );
+  }
+}
